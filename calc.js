3a0df8cacb3959d4c76ff466ba93837f4974d401
Akshai Kumar Jaagaraganti <154488331+Akkiakshai@users.noreply.github.com>
2026-03-13 09:35:50 +0530

    calc.js

--- calc.js
+++ calc.js
@@ -1,19 +1,13 @@
-// Simple Calculator using switch case with user input
-
 const readline = require("readline");
-
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
-
 rl.question("Enter first number: ", function(num1) {
   rl.question("Enter operator (+, -, *, /): ", function(op) {
     rl.question("Enter second number: ", function(num2) {
-
       num1 = Number(num1);
       num2 = Number(num2);
-
       switch (op) {
         case "+":
           console.log("Result:", num1 + num2);
@@ -30,7 +24,6 @@ rl.question("Enter first number: ", function(num1) {
         default:
           console.log("Invalid operator");
       }
-
       rl.close();
     });
   });
